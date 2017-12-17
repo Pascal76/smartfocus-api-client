b129b31fce57a51d368d2f48ac6a1b882da48abd
Pascal76 <pascal@halogen-consulting.com>
2017-12-17 12:24:36 +0100

    Update Client.php

--- src/Client.php
+++ src/Client.php
@@ -3,20 +3,14 @@
 namespace L0rD59\Smartfocus;
 
 use GuzzleHttp\Command\Guzzle\GuzzleClient;
-use GuzzleHttp\Subscriber\Log\LogSubscriber;
-use Psr\Log\LoggerInterface;
 
 class Client extends GuzzleClient
 {
-    public function __construct(LoggerInterface $logger = null)
+    public function __construct()
     {
         $smartfocusApiDescription = include_once 'resources/smartfocus-api-description.php';
 
         $guzzleClient = new \GuzzleHttp\Client();
-
-        if (!is_null($logger)) {
-            $guzzleClient->getEmitter()->attach(new LogSubscriber($logger));
-        }
 
         parent::__construct($guzzleClient, $smartfocusApiDescription);
     }
